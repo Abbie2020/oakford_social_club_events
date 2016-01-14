27fb9d3d3e72750388c499d58fe3bec4fbc47bd3
Abbie2020 <abbie_is@hotmail.com>
2016-01-14 20:47:38 +0000

    Included SQL DELETE to clear table before storing new data

--- scraper.py
+++ scraper.py
@@ -4,27 +4,26 @@ import scraperwiki
 import time
 import string
 from dateutil.parser import parse
+from datetime import date
 
 def scrape_events():
     page = requests.get('https://www.oakfordsocialclub.com/events')
     tree = html.fromstring(page.text)
     sections = tree.xpath('//div[@class="accordion--accordion"]/section')
-    count = 0
     for section in sections:
         h1 = section.xpath('.//h1/text()')
         title = h1[0].split('- ',1)[0]
-        date = h1[0].split('- ',1)[1]
-        date = parse(date)
-        id = date.strftime('%Y%m%d')
-        date = date.strftime('%Y-%m-%d')
+        event_date = h1[0].split('- ',1)[1]
+        event_date = parse(event_date)
+        id = event_date.strftime('%Y%m%d')
+        event_date = event_date.strftime('%Y-%m-%d')
         event_time = section.xpath('.//div[@class="text--only"]/p[1]/text()')
         description = section.xpath('.//div[@class="text--only"]/p[2]/text()')
-        count += 1
         
         data = {
             'id': 'oak' + str(id),
             'title': title,
-            'date': date,
+            'date': event_date,
             'time': event_time[0],
             'description': description[0],
             'venue': 'Oakford Social Club',
@@ -35,4 +34,5 @@ def scrape_events():
         scraperwiki.sqlite.save(unique_keys = ['id'], data = data)
         time.sleep(0.5)
     
+scraperwiki.sqlite.execute("DELETE FROM swdata")
 scrape_events()
